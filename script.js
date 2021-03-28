ca6c65fa441898bb240d1660d3ea55a572ddbc13
milosleng <milosleng@gmail.com>
2021-03-28 18:40:17 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+//POPULATING AN ARRAY
+//map
+const newArr = new Array(15)
+  .fill(null)
+  .map(() => ({ name: 'Milosko', secondName: { hello: 'Wolrd' } }));
+console.log(newArr);
+
+//loop
+const items = new Array(7);
+for (let i = 0; i < items.length; i++) {
+  items[i] = [1, 2, 3, 4];
+}
+
+console.log(items);
+
+//Array.from
+let filledArray = Array.from({ length: 10 }, () => ({ name: 'Milos' }));
+console.log(filledArray);
+
+//Spread operator
+
+let fillArray = [...Array.from(10)].map(() => ({ age: 30 }));
