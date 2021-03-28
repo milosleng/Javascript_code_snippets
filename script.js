a171630863e95822eb6303ca31aab857e3fd4455
milosleng <milosleng@gmail.com>
2021-03-28 19:24:37 +0200

    Add object descructuring

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//TODO:ARRAYS
 //POPULATING AN ARRAY
 //map
 const newArr = new Array(15)
@@ -20,3 +21,36 @@ console.log(filledArray);
 //spread operator
 
 let fillArray = [...Array.from(10)].map(() => ({ age: 30 }));
+
+//min/max of an array
+Math.max(3, 4, 20);
+
+//with arr - ES5
+const nums = [4, 5, 1, 3, 3];
+Math.min.apply(Math, nums); //1
+
+//descructuring
+Math.max(...nums);
+
+//TODO:OBJECTS
+//destructuring
+const myVacation = {
+  startDate: '05-01-2020',
+  endDate: '10-01-2020',
+  citiesVisited: ['Roma', 'Torino', 'Naples'],
+  stops: {
+    Roma: {
+      friendsMade: 4,
+      barsVisited: 3,
+    },
+    Naples: {
+      friendsMade: 10,
+      barsVisited: 5,
+    },
+  },
+};
+
+const { startDate, endDate, citiesVisited, stops } = myVacation;
+const { Roma: roma, Naples: naples } = stops;
+
+console.log(roma, naples);
